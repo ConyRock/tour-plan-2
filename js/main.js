8a5e54965ec30e0712a1182eb10a02c24d8de61f
ConyRock <therock.cony@gmail.com>
2023-04-09 20:44:01 +0500

    added: styles for modal section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// slider
 const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -22,10 +23,36 @@ const reviewsSwiper = new Swiper('.reviews-slider', {
 
 });
 
+// menu button
 const btn = document.querySelector('[data-menu-button]')
 
 btn.addEventListener('click', () => {
   document
     .querySelector('.navbar-bottom')
-    .classList.toggle('navbar-bottom_visible')  
+    .classList.toggle('navbar-bottom_visible')
+})
+
+
+// modal booking 
+const modalOpen = document.querySelector('[data-modal-open]')
+
+modalOpen.addEventListener('click', () => {
+  document.querySelector('.modal__overlay').classList.add('modal__overlay_visible')
+  document.querySelector('.modal__dialog').classList.add('modal__dialog_visible')
+})
+
+// close button
+const modalCloseButton = document.querySelector('[data-modal-close-button]')
+
+modalCloseButton.addEventListener('click', () => {
+  document.querySelector('.modal__overlay').classList.remove('modal__overlay_visible')
+  document.querySelector('.modal__dialog').classList.remove('modal__dialog_visible')
+})
+
+// close overlay
+const modalCloseOverlay = document.querySelector('[data-modal-close-overlay]')
+
+modalCloseOverlay.addEventListener('click', () => {
+  document.querySelector('.modal__overlay').classList.remove('modal__overlay_visible')
+  document.querySelector('.modal__dialog').classList.remove('modal__dialog_visible')
 })
